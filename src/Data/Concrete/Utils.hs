f1fc710c7a083f41a318ef31b2d6ad663fa69388
Tom Lippincott <tom.lippincott@gmail.com>
2016-11-25 08:24:35 -0500

    Fixed extended dependency.

--- src/Data/Concrete/Utils.hs
+++ src/Data/Concrete/Utils.hs
@@ -1,21 +1,11 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Data.Concrete.Utils
        (
-         jsonToCommunication
        ) where
 
 import GHC.Generics
 import qualified Data.Concrete as C
 import Data.Concrete (Communication)
 import Data.Text
-import Data.Aeson
 import Data.ByteString.Lazy
 import Data.Map
-
-instance FromJSON Communication where
-  parseJSON (Object v) = return C.default_Communication
-
-jsonToCommunication :: ByteString -> IO (Maybe Communication)
-jsonToCommunication t = do
-  return $ (decode t :: Maybe Communication)
-
