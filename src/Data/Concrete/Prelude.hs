beb7ae7bf85885ea251471a72617cd2bf87945dd
Tom Lippincott <tom@calculon-minor>
2017-11-30 15:28:21 -0500

    Super-prelude.

---
+++ src/Data/Concrete/Prelude.hs
@@ -0,0 +1,51 @@
+module Data.Concrete.Prelude ( module Data.Concrete.Autogen.Communication_Types
+                             , module Data.Concrete.Autogen.Metadata_Types
+                             , module Data.Concrete.Autogen.Language_Types
+                             , module Data.Concrete.Autogen.Spans_Types
+                             , module Data.Concrete.Autogen.Structure_Types
+                             , module Data.Concrete.Autogen.Uuid_Types
+                             , module Data.Concrete.Autogen.Entities_Types
+                             , module Data.Concrete.Autogen.Cluster_Types
+                             , module Data.Concrete.Autogen.Email_Types
+                             , module Data.Concrete.Autogen.Audio_Types
+                             , module Data.Concrete.Autogen.Access_Types
+                             , module Data.Concrete.Autogen.Graph_Types
+                             , module Data.Concrete.Autogen.Learn_Types
+                             , module Data.Concrete.Autogen.Linking_Types
+                             , module Data.Concrete.Autogen.Nitf_Types
+                             , module Data.Concrete.Autogen.Results_Types
+                             , module Data.Concrete.Autogen.Twitter_Types
+
+                             , module Data.Concrete.Autogen.Lens.Section
+                             , module Data.Concrete.Autogen.Lens.Communication
+                             , module Data.Concrete.Autogen.Lens.AnnotationMetadata                             
+                             ) where
+
+import Data.Concrete.Autogen.Communication_Types
+import Data.Concrete.Autogen.Structure_Types
+import Data.Concrete.Autogen.Spans_Types
+import Data.Concrete.Autogen.Metadata_Types
+import Data.Concrete.Autogen.Uuid_Types
+import Data.Concrete.Autogen.Language_Types
+import Data.Concrete.Autogen.Entities_Types
+import Data.Concrete.Autogen.Cluster_Types
+import Data.Concrete.Autogen.Email_Types
+import Data.Concrete.Autogen.Audio_Types
+import Data.Concrete.Autogen.Access_Types
+import Data.Concrete.Autogen.Graph_Types
+import Data.Concrete.Autogen.Learn_Types
+import Data.Concrete.Autogen.Linking_Types
+import Data.Concrete.Autogen.Nitf_Types
+import Data.Concrete.Autogen.Results_Types
+import Data.Concrete.Autogen.Twitter_Types
+
+import Data.Concrete.Autogen.Lens.Communication
+import Data.Concrete.Autogen.Lens.Section
+import Data.Concrete.Autogen.Lens.Sentence
+import Data.Concrete.Autogen.Lens.Token
+import Data.Concrete.Autogen.Lens.TaggedToken
+import Data.Concrete.Autogen.Lens.LanguageIdentification
+import Data.Concrete.Autogen.Lens.AnnotationMetadata
+import Data.Concrete.Autogen.Lens.CommunicationMetadata
+import Data.Concrete.Autogen.Lens.TextSpan
+import Data.Concrete.Autogen.Lens.UUID
